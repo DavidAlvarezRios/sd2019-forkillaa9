bffe24d2fb96a570d5d21c437ac8fca59ffdd7b9
DavidAlvarezRios <dalvarezrios@gmail.com>
2019-05-24 23:42:26 +0200

    code error 500 solved

--- PracticaWeb/settings.py
+++ PracticaWeb/settings.py
@@ -78,25 +78,16 @@ WSGI_APPLICATION = 'PracticaWeb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db.sqlite3',
-        #'USER': 'user',
-        #'PASSWORD': '',
-        #'HOST': 'localhost',
-        #'PORT': '',
-        #'CONN_MAX_AGE': 500
+        'NAME': 'sd2019-forkilla',
+        'USER': 'user',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+        'CONN_MAX_AGE': 500
 
-    }
-}
-"""
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
